a6c6aec8ae0f13be9661a592319491779b949644
gkjuanmao <gkjuanmao@gmail.com>
2022-08-31 01:11:05 +0800

    dialog

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <HelloWorld/> -->
+    <div class="dialog">
+      <button @click="visible = true">打开弹窗</button>
+
+      <simple-dialog 
+          :visible.sync="visible"
+          :width="width"
+          :title="title"
+          @close="onClose"
+          @open="onOpen">
+        <template v-slot:title>
+          <div>⾃定义标题</div>
+        </template>
+        <template>
+          <div>主体内容，随便写点什么...</div>
+        </template>
+      </simple-dialog>
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+import simpleDialog from './components/simpleDialog.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    simpleDialog
+  },
+  data() {
+    return {
+      visible: false,
+      width: '50%',
+      title: ""
+    };
+  },
+  methods: {
+    onClose() {
+      console.log("关闭弹窗回调")
+    },
+    onOpen() {
+      console.log("打开弹窗回调")
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/simpleDialog.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="dialog__wrapper" v-if="visible" @click.self="close">
+    <div class="dialog" :style="{width: width}">
+      <div class="dialog__header">
+        <slot name="title">
+          <span class="dialog__title">{{ title }}</span>
+        </slot>
+        <button class="dialog__headerbtn" @click="close">X</button>
+      </div>
+      <div class="dialog__body">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'simpleDialog',
+  data() {
+    return {}
+  },
+  props: {
+    title: {
+      type: String,
+      default: "提示",
+    },
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    width: {
+      type: String,
+      default: "30%"
+    }
+  },
+  methods: {
+    close() {
+      this.$emit('update:visible',false);
+    },
+  }
+}
+</script>
+
+<style scoped>
+  .dialog__wrapper {
+    background: rgba(0,0,0, 0.8);
+    position: fixed;
+    z-index: 200;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-content: center;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+  }
+  .dialog {
+    background: white;
+    width: 40%;
+    height: 50%;
+    z-index: 300;
+    border-radius: 10px;
+  }
+  .dialog__header {
+    height: auto;
+    padding-top: 5px;
+    display: flex;
+    align-content: flex-start;
+    position: relative;
+    justify-content: space-around;
+  }
+  .dialog__headerbtn {
+    position: absolute;
+    right: 5px;
+    cursor: pointer;
+    border: none;
+    background: none;
+    outline: none;
+    font-size: 17px;
+  }
+  .dialog__body {
+    height: 65%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
